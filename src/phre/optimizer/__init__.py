73b251f3230d6bdf1bd428b4d05a0e9266b2aa08
zhengp0 <zhengp@uw.edu>
2020-06-06 14:04:29 -0700

    add ADMM solver and incorporate into the examples.

--- src/phre/optimizer/__init__.py
+++ src/phre/optimizer/__init__.py
@@ -1,2 +1,3 @@
 from .relax_split import RelaxSplit
+from .admm import ADMM
 from .initialization import find_initial_image
--- src/phre/optimizer/admm.py
+++ src/phre/optimizer/admm.py
@@ -1,34 +1,33 @@
 """
-    relax_split
-    ~~~~~~~~~~~
+    admm
+    ~~~~
 """
 from typing import Tuple, Union
 import numpy as np
 from phre.utils import distance
 
 
-class RelaxSplit:
-    """Relax and split optimizer.
+class ADMM:
+    """ADMM Optimizer.
     """
 
     def __init__(self,
                  obs_mat: np.ndarray,
                  obs: np.ndarray,
-                 nu: float = 1.0):
-        """Constructor of relax and split solver.
+                 rho: float = 1.0):
+        """Constructor of ADMM solver.
 
         Args:
             obs_mat (np.ndarray):
                 Observation mapping, from original image to observation.
             obs (np.ndarray): Observations.
-            nu (float, optional):
-                Hyper-parameter of relax and split solver, controls how close
-                to the original (before relax) formulation. The smaller the `nu`
-                the closer to the original problem. Defaults to 1.0.
+            rho (float, optional):
+                Hyper-parameter of ADMM algorithm, regularization for the
+                Lagrangian. Defualt to 1.0.
         """
         self.obs_mat = obs_mat
         self.obs = obs
-        self.nu = nu
+        self.rho = rho
 
         self.num_obs = self.obs_mat.shape[0]
         self.image_size = self.obs_mat.shape[1]
@@ -38,6 +37,7 @@ class RelaxSplit:
     def phase_retrieval(self,
                         init_x: Union[np.ndarray, None] = None,
                         init_w: Union[np.ndarray, None] = None,
+                        init_d: Union[np.ndarray, None] = None,
                         tol: float = 1e-6,
                         max_iter: int = 100,
                         verbose: bool = False) -> np.ndarray:
@@ -50,6 +50,9 @@ class RelaxSplit:
             init_w (Union[np.ndarray, None], optional):
                 Initialization of the measurements of the optimization problem.
                 If `None`, start from `self.obs`. Defualts to None.
+            init_d (Union[np.ndarray, None], optional):
+                Initialization of the dual variable. If `None`, start from zero
+                vector. Defaults to None.
             tol (float, optional):
                 Tolerance for convergence of the optimization problem.
             max_iter (int, optional):
@@ -61,20 +64,24 @@ class RelaxSplit:
         Returns:
             np.ndarray: Final result.
         """
-        x, w = self.initialize_vars(init_x=init_x, init_w=init_w)
+        x, w, d = self.initialize_vars(init_x=init_x,
+                                       init_w=init_w,
+                                       init_d=init_d)
 
         for iter_counter in range(1, max_iter + 1):
-            x_new = self.step_x(w)
-            w_new = self.step_w(x_new)
+            x_new = self.step_x(w, d)
+            w_new = self.step_w(x_new, d)
+            d_new = self.step_d(x_new, w_new, d)
 
-            err = distance((x, w), (x_new, w_new),
+            err = distance((x, w, d), (x_new, w_new, d_new),
                            rel=True, check_inputs=False)
 
             np.copyto(x, x_new)
             np.copyto(w, w_new)
+            np.copyto(d, d_new)
 
             if verbose:
-                obj = self.objective(x, w)
+                obj = self.objective(x, w, d)
                 print(f"iter {iter_counter:5}, obj {obj: .2e}, err {err:.2e}")
 
             if err < tol:
@@ -82,24 +89,27 @@ class RelaxSplit:
 
         return x
 
-    def objective(self, x: np.ndarray, w: np.ndarray) -> float:
-        """Objective function value.
+    def objective(self, x: np.ndarray, w: np.ndarray, d: np.ndarray) -> float:
+        """Lagrangian objective function
 
         Args:
-            x (np.ndarray): Image variable.
-            w (np.ndarray): Pseudo observation variable.
+            x (np.ndarray): Image vector.
+            w (np.ndarray): Pesudo image obersvation vector.
+            d (np.ndarray): Dual variable.
 
         Returns:
-            float: Objective function value.
+            float: Lagrangian objective value.
         """
         v = self.obs_mat.dot(x)
         val = np.sum(np.abs(np.abs(w) - self.obs))
-        val += 0.5*np.sum((v - w)**2)/self.nu
+        val += 0.5*self.rho*np.sum((v - w)**2)
+        val += self.rho*d.dot(v - w)
         return val
 
     def initialize_vars(self,
                         init_x: Union[np.ndarray, None] = None,
-                        init_w: Union[np.ndarray, None] = None) -> Tuple[np.ndarray, np.ndarray]:
+                        init_w: Union[np.ndarray, None] = None,
+                        init_d: Union[np.ndarray, None] = None) -> Tuple[np.ndarray, np.ndarray]:
         """Intialize variables.
 
         Args:
@@ -109,10 +119,13 @@ class RelaxSplit:
             init_w (Union[np.ndarray, None], optional):
                 Initialization of the measurements of the optimization problem.
                 If `None`, start from `self.obs`. Defualts to None.
+            init_d (Union[np.ndarray, None], optional):
+                Initialization of the dual variable. If `None`, start from zero
+                vector. Defaults to None.
 
         Returns:
             Tuple[np.ndarray, np.ndarray]:
-                Initial x and w variable.
+                Initial x, w and d variable.
         """
         # initialization
         if init_x is None:
@@ -137,39 +150,65 @@ class RelaxSplit:
                 "be consistent with num_obs ({self.num_obs})"
             )
 
-        return init_x, init_w
+        if init_d is None:
+            init_d = np.zeros(self.num_obs)
+        else:
+            init_d = init_d.copy()
 
-    def step_x(self, w: np.ndarray) -> np.ndarray:
+        if init_d.size != self.num_obs:
+            raise ValueError(
+                f"Invalid initial value for d, size ({init_d.size}) should"
+                "be consistent with num_obs ({self.num_obs})"
+            )
+
+        return init_x, init_w, init_d
+
+    def step_x(self, w: np.ndarray, d: np.ndarray) -> np.ndarray:
         """Optimization x step.
 
         Args:
             w (np.ndarray): Pseudo observation vector.
+            d (np.ndarray): Dual varaible.
 
         Returns:
-            np.ndarray: Optimal image vector based on `w`.
+            np.ndarray: Optimal image at the step.
         """
-        return np.linalg.solve(self._cached_mat, self.obs_mat.T.dot(w))
+        return np.linalg.solve(self._cached_mat, self.obs_mat.T.dot(w - d))
 
-    def step_w(self, x: np.ndarray) -> np.ndarray:
+    def step_w(self, x: np.ndarray, d: np.ndarray) -> np.ndarray:
         """Optimization w step.
 
         Args:
             x (np.ndarray): Image vector.
+            d (np.ndarray): Dual variable.
 
         Returns:
-            np.ndarray: Optimal pseudo observation vector.
+            np.ndarray: Optimal pseudo observation vector at the step.
         """
-        v = self.obs_mat.dot(x)
+        v = self.obs_mat.dot(x) + d
         sign_v = np.sign(v)
         v *= sign_v
 
         w = self.obs.copy()
-        r_index = v > self.obs + self.nu
-        l_index = v < self.obs - self.nu
+        r_index = v > self.obs + 1.0/self.rho
+        l_index = v < self.obs - 1.0/self.rho
 
-        w[r_index] = v[r_index] - self.nu
-        w[l_index] = v[l_index] + self.nu
+        w[r_index] = v[r_index] - 1.0/self.rho
+        w[l_index] = v[l_index] + 1.0/self.rho
 
         w *= sign_v
 
         return w
+
+    def step_d(self, x: np.ndarray, w: np.ndarray, d: np.ndarray) -> np.ndarray:
+        """Optimiation d step.
+
+        Args:
+            x (np.ndarray): Image vector.
+            w (np.ndarray): Pesudo image obersvation vector.
+            d (np.ndarray): Dual variable.
+
+        Returns:
+            np.ndarray: Optimal dual variable at the step.
+        """
+        return d + self.obs_mat.dot(x) - w
